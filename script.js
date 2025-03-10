cc0ca067432171e07011e47a22a45547b73b230e
mukondi29 <808713@students.wits.ac.za>
2025-03-10 17:18:33 +0200

    Changes

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+    document.getElementById("name_button").addEventListener('click',function (){
+
+    
+    const url="https://restcountries.com/"
+    try{
+        const response = await fetch(url)
+        if (!response.ok){
+            throw new Error("Error")
+        }
+        const json = await response.json();
+        return response.json();
+    }catch (error){
+        console.error(error.message);
+    }
+    .then((data)=>{
+        if(data &&data[0]){
+            const country =data[0];
+            document.getElementById("countryName").textContent =`Country: ${country.name.common}`;
+            document.getElementById("countryPop").textContent =`Population: ${country.population.toLocaleString()}`;
+            document.getElementById("countryRegion").textContent =`Region: ${country.region}`;
+            document.getElementById("countryFlag").textContent =`Flag: ${country.flags.svg}`;
+
+            const neighbours =country.neighbours ? country.neighbours.join(', ') : 'None';
+            document.getElementById('neighbour').textContent =`Neighbours: ${neighbours}`;
+        }
+    }
+    });
+    
+    // var country_name =document.getElementById("name")
+    // console.log(country_name)
+
+    
+    // const input = document.getElementById("name")
+    // const button = document.getElementById("name_button")
+    // var country_name
+    // country_name = input
+    // console.log(country_name)
+    // button.addEventListener('click',updateCountryName)
+    // function updateCountryName(){
+    //     country_name = input
+    // }
+ 
 
